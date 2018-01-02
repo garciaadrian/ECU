eb968f490f7a66871cc675b74f0885918f634551
Adrian Garcia Cruz <garcia.adrian.cruz@gmail.com>
2018-01-01 22:46:53 -0500

    Use windeployqt to handle Qt dependencies

--- cmake/RuntimeDependencies.cmake
+++ cmake/RuntimeDependencies.cmake
@@ -11,6 +11,8 @@
 # Full dependencies for self-contained packages
 # 
 
+# TODO:(garciaadrian): Move Qt install code here?
+
 if(OPTION_SELF_CONTAINED)
 
     # Install 3rd-party runtime dependencies into runtime-component
--- extern/ExternalLibs.cmake
+++ extern/ExternalLibs.cmake
@@ -16,7 +16,7 @@ include(${extern_dir}/websocketpp.cmake)
 include(${extern_dir}/openssl.cmake)
 include(${extern_dir}/asio.cmake)
 
-# CMAKE
+# Qt5
 
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 set(CMAKE_AUTOMOC ON)
@@ -24,3 +24,24 @@ set(CMAKE_AUTOUIC ON)
 set(CMAKE_AUTORCC ON)
 
 find_package(Qt5 COMPONENTS Core Widgets Gui REQUIRED)
+
+if(Qt5_FOUND AND WIN32 AND TARGET Qt5::qmake AND NOT TARGET Qt5::windeployqt)
+  get_target_property(_qt5_qmake_location Qt5::qmake IMPORTED_LOCATION)
+
+  execute_process(
+    COMMAND "${_qt5_qmake_location}" -query QT_INSTALL_PREFIX
+    RESULT_VARIABLE return_code
+    OUTPUT_VARIABLE qt5_install_prefix
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+  set(imported_location "${qt5_install_prefix}/bin/windeployqt.exe")
+
+  if(EXISTS ${imported_location})
+    add_executable(Qt5::windeployqt IMPORTED)
+
+    set_target_properties(Qt5::windeployqt PROPERTIES
+      IMPORTED_LOCATION ${imported_location}
+      )
+  endif()
+endif()
